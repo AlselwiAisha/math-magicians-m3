d09c3bc5ede69838d1b7a17d99cc594792ed5461
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-09-13 12:24:40 +0300

    Add calculate and event

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import ClcScrn from './ClcScrn';
 import Btn from './Btn';
+import calculate from '../logic/calculate';
 
 const Calculator = () => {
+  const [result, setResult] = useState({});
+  const { total, next, operation } = result;
+
+  const onClick = (e) => {
+    const btn = e.target.innerHTML;
+    setResult(calculate(result, btn));
+  };
   const btnValues = [
     ['AC', '+/-', '%', '÷'],
     ['7', '8', '9', 'x'],
@@ -11,14 +19,16 @@ const Calculator = () => {
     ['1', '2', '3', '+'],
     ['0', '.', '='],
   ];
+
   return (
     <div className="clcBckgrnd">
-      <ClcScrn value="0" />
+      <ClcScrn value={total || next || operation || 0} />
       <div className="btnsPad">
         {btnValues.flat().map((row) => (
           <Btn
             key={row.id}
             value={row}
+            onClick={onClick}
           />
         ))}
       </div>
