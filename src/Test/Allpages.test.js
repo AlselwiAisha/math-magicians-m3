44431e7dca44790fcd860ea72e8154fcbeecf1ea
unknown <lemmayonas@gmail.com>
2023-09-21 10:27:22 +0300

    add a testing case

---
+++ src/Test/Allpages.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Home from '../AllPages/Home';
+import Calculator from '../AllPages/Calculator';
+import Quote from '../AllPages/Quote';
+describe('Home AllPages', () => {
+  it('should match the snapshot', () => {
+    const { container } = render(<Home />);
+    expect(container).toMatchSnapshot();
+  });
+});
+describe('Calculator AllPages', () => {
+  it('should match the snapshot', () => {
+    const { container } = render(<Calculator />);
+    expect(container).toMatchSnapshot();
+  });
+});
+describe('Quote AllPages', () => {
+  it('should match the snapshot', () => {
+    const { container } = render(<Quote />);
+    expect(container).toMatchSnapshot();
+  });
+});
